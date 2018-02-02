9df5f60555d6ca1f631a2bbabfa1de0de30dbec3
Tim Sargent <timothy.sargent@pcc.edu>
2018-02-02 15:03:20 -0800

    Print date

--- src/edu/pcc/cis/timsargent/timsargent.java
+++ src/edu/pcc/cis/timsargent/timsargent.java
@@ -1,8 +1,12 @@
 package edu.pcc.cis.timsargent;
 
+import java.util.Date;
+
 public class timsargent {
     public static void main(String[] args) {
         System.out.println("This is a");
         System.out.println("print statement");
+        System.out.println(new Date());
     }
 }
+
