c9746ebc0059c890af76c62b606105d0b34600ca
Tim Sargent <timothy.sargent@pcc.edu>
2018-02-02 14:59:21 -0800

    Added main method that prints statement

--- src/edu/pcc/cis/timsargent/timsargent.java
+++ src/edu/pcc/cis/timsargent/timsargent.java
@@ -1,4 +1,8 @@
 package edu.pcc.cis.timsargent;
 
 public class timsargent {
+    public static void main(String[] args) {
+        System.out.println("This is a");
+        System.out.println("print statement");
+    }
 }
